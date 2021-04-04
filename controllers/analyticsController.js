0cba7c75ab491dc07024dca73db643e6c292a0d7
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2021-04-04 21:47:04 +0700

    CP-21#Clusterized Analytics Response

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,8 +1,7 @@
 const { ErrorHandler } = require('../lib/error');
 const BaseController = require('./baseController');
-const spawn = require('child_process').spawn;
-const path = require('path');
 const axios = require('axios');
+const EPSILON = 0.00000000000000000001;
 
 class AnalyticsController extends BaseController {
     constructor() {
@@ -23,11 +22,7 @@ class AnalyticsController extends BaseController {
 
                 let response = await axios.post(process.env.AI_SERVICE, requestBody);
                 if (response.status === 200) {
-                    let result = {
-                        pair: response.data.Pair,
-                        substring: response.data.Substring,
-                        similarity: response.data.Similarity
-                    }
+                    let result = this.clusterizedAnalyticResponse(response.data);
                     res.status(200).json(super.createSuccessResponse(result));
                 }
             } else {
@@ -39,6 +34,71 @@ class AnalyticsController extends BaseController {
         }
     }
 
+    clusterizedAnalyticResponse(rawResponse) {
+        let clusterized = [
+            {
+                clusterMin: "0%",
+                clusterMax: "25%",
+                pairingCount: 0,
+                clusterPairIndex: [],
+                clusterSimilarities: [],
+                clusterSubstrings: [],
+            },
+            {
+                clusterMin: "25%",
+                clusterMax: "50%",
+                pairingCount: 0,
+                clusterPairIndex: [],
+                clusterSimilarities: [],
+                clusterSubstrings: [],
+            },
+            {
+                clusterMin: "50%",
+                clusterMax: "75%",
+                pairingCount: 0,
+                clusterPairIndex: [],
+                clusterSimilarities: [],
+                clusterSubstrings: [],
+            },
+            {
+                clusterMin: "75%",
+                clusterMax: "100%",
+                pairingCount: 0,
+                clusterPairIndex: [],
+                clusterSimilarities: [],
+                clusterSubstrings: [],
+            }
+        ];
+
+        rawResponse.Similarity.forEach((item, index) => {
+            let x = Math.round(parseFloat(item) * 100 ) / 100;
+            console.log(0.0 <= x <= 0.25)
+            if (x <= 0.25) {
+                clusterized[0].pairingCount += 1;
+                clusterized[0].clusterPairIndex.push(rawResponse.Pair[index]);
+                clusterized[0].clusterSubstrings.push(rawResponse.Substring[index]);
+                clusterized[0].clusterSimilarities.push(item);
+            } else if (0.25 < x <= 0.50) {
+                clusterized[1].pairingCount += 1;
+                clusterized[1].clusterPairIndex.push(rawResponse.Pair[index]);
+                clusterized[1].clusterSubstrings.push(rawResponse.Substring[index]);
+                clusterized[1].clusterSimilarities.push(item);
+            } else if (0.50 < x <= 0.75) {
+                clusterized[2].pairingCount += 1;
+                clusterized[2].clusterPairIndex.push(rawResponse.Pair[index]);
+                clusterized[2].clusterSubstrings.push(rawResponse.Substring[index]);
+                clusterized[2].clusterSimilarities.push(item);
+            } else {
+                clusterized[3].pairingCount += 1;
+                clusterized[3].clusterPairIndex.push(rawResponse.Pair[index]);
+                clusterized[3].clusterSubstrings.push(rawResponse.Substring[index]);
+                clusterized[3].clusterSimilarities.push(item);
+            }
+        });
+
+        return clusterized;
+    }
+
     validateClientInput(documents, threshold) {
         if (!documents) {
             throw new ErrorHandler("documents must be provided");
@@ -48,6 +108,14 @@ class AnalyticsController extends BaseController {
             throw new ErrorHandler("documents must have at least 2 items");
         }
     }
+
+    isALessThanB(A, B) {
+		return (A - B < EPSILON) && (Math.abs(A - B) > EPSILON);
+	};
+
+	isAMoreThanB(A, B) {
+		return (A - B > EPSILON) && (Math.abs(A - B) > EPSILON);
+	};
 }
 
 module.exports = new AnalyticsController();
